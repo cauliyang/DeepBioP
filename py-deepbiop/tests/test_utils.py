f985cd08eb550e3aa4f8f2bdeb29076e8df8d27e
Yangyang Li <yangyang.li@northwestern.edu>
2024-08-04 22:00:13 -0500

    ci: Add step to print GitHub tag in release workflow

--- py-deepbiop/tests/test_utils.py
+++ py-deepbiop/tests/test_utils.py
@@ -1,6 +1,6 @@
-import deepbiop
+from deepbiop import utils
 
 
 def test_hight_targets():
-    result = deepbiop.utils.highlight_targets("ACTGAACCGAGATCGAGTG", [(0, 3), (7, 10)])
+    result = utils.highlight_targets("ACTGAACCGAGATCGAGTG", [(0, 3), (7, 10)])
     print(result)
