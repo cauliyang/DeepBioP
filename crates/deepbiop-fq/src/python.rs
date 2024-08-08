bfbb498be10830f8606fdce931e2cb8c88232507
Yangyang Li <yangyang.li@northwestern.edu>
2024-08-07 22:03:02 -0500

    refactor: Improve submodule registration method

--- crates/deepbiop-fq/src/python.rs
+++ crates/deepbiop-fq/src/python.rs
@@ -507,7 +507,7 @@ pub fn register_fq_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
         .py()
         .import_bound("sys")?
         .getattr("modules")?
-        .set_item(sub_module_name, &child_module)?;
+        .set_item(sub_module_name.split('.').last().unwrap(), &child_module)?;
 
     parent_module.add_submodule(&child_module)?;
     Ok(())
