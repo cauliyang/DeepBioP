35129929f60e4054ccd498e280742f3d6a484a9e
Yangyang Li <yangyang.li@northwestern.edu>
2024-08-19 15:00:26 -0500

    feat: Add pyo3-stub-gen and pyo3-build-config dependencies

--- py-deepbiop/src/lib.rs
+++ py-deepbiop/src/lib.rs
@@ -6,6 +6,10 @@ use deepbiop_bam::python::register_bam_module;
 use deepbiop_fq::python::register_fq_module;
 use deepbiop_utils::python::register_utils_module;
 
+use pyo3_stub_gen::define_stub_info_gatherer;
+use pyo3_stub_gen::derive::*;
+
+#[gen_stub_pyfunction]
 #[pyfunction]
 fn add(a: usize, b: usize) -> usize {
     a + b
@@ -24,3 +28,6 @@ fn deepbiop(m: &Bound<'_, PyModule>) -> PyResult<()> {
 
     Ok(())
 }
+
+// Define a function to gather stub information.
+define_stub_info_gatherer!(stub_info);
