ded91ab4755345ce2ed27269269ab17f73080165
Yangyang Li <yangyang.li@northwestern.edu>
2024-08-20 15:28:02 -0500

    refactor: Update dependencies in Cargo.toml and set_up_threads in cli.rs

--- crates/deepbiop-cli/src/cli.rs
+++ crates/deepbiop-cli/src/cli.rs
@@ -1,2 +1,15 @@
 pub mod chimeric_count;
 pub use chimeric_count::*;
+
+use anyhow::Result;
+
+// Set up threads only once, using the common_opts from the top-level Cli struct
+pub fn set_up_threads(threads: Option<usize>) -> Result<()> {
+    log::info!("Threads number: {:?}", threads.unwrap());
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(threads.unwrap())
+        .build_global()
+        .unwrap();
+    Ok(())
+}
--- crates/deepbiop-cli/src/main.rs
+++ crates/deepbiop-cli/src/main.rs
@@ -1,32 +1,15 @@
-use clap::{Command, CommandFactory, Parser, Subcommand, ValueHint};
+use clap::{Command, CommandFactory, Parser, Subcommand};
 use env_logger::Builder;
 use human_panic::setup_panic;
 use log::info;
 use log::LevelFilter;
+use std::fmt::Display;
 use std::io::Result;
-use std::{fmt::Display, path::PathBuf};
 
 use clap_complete::{generate, Generator, Shell};
 use std::io;
 
 mod cli;
-
-#[derive(clap::Args, Debug, Clone)]
-#[command(about = None, long_about = None)]
-struct CommonOpts {
-    /// threads number
-    #[arg(short, long, default_value = "2")]
-    threads: Option<usize>,
-}
-
-#[derive(clap::Args, Debug)]
-pub struct WithCommonOpts<T: clap::Args> {
-    #[command(flatten)]
-    common_opts: CommonOpts,
-
-    #[command(flatten)]
-    inner: T,
-}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -40,16 +23,13 @@ struct Cli {
     command: Option<Commands>,
 
     #[command(flatten)]
-    common_opts: CommonOpts,
-
-    #[command(flatten)]
     verbose: clap_verbosity_flag::Verbosity,
 }
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
     /// Count chimeric reads in a BAM file.
-    CountChimeric(WithCommonOpts<cli::CountChimeric>),
+    CountChimeric(cli::CountChimeric),
 }
 
 impl Display for Commands {
@@ -62,17 +42,6 @@ impl Display for Commands {
 
 fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
     generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
-}
-
-fn set_up_threads(threads: Option<usize>) -> Result<()> {
-    log::info!("Threads number: {:?}", threads.unwrap());
-
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(threads.unwrap())
-        .build_global()
-        .unwrap();
-
-    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -102,12 +71,9 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    // Set up threads only once, using the common_opts from the top-level Cli struct
-    set_up_threads(cli.common_opts.threads)?;
-
     match &cli.command {
         Some(Commands::CountChimeric(count_chimeric)) => {
-            println!("{:?}", count_chimeric);
+            println!("{:?}", count_chimeric.run());
         }
 
         None => {
